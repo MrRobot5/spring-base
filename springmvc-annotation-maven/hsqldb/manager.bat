f8cdee69f39eb3ff11cdab9577696c2b1dc3b072
yangpan23 <yangpan3@jd.com>
2016-03-02 10:35:35 +0800

    remove directory

--- springmvc-annotation-maven/hsqldb/manager.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem Æô¶¯hsqldb manager
-javaw -classpath hsqldb.jar org.hsqldb.util.DatabaseManagerSwing -url jdbc:hsqldb:hsql://localhost/xda
--- springmvc-annotation-maven/hsqldb/server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem Æô¶¯hsqldb server
-java -classpath hsqldb.jar org.hsqldb.Server -database.0 xda -dbname.0 xda
